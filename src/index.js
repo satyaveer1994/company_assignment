708a33f889662cfc0fd9108ec76ab469848b0917
satyaveer1994 <satyaveerjat1994@gmail.com>
2023-03-31 23:43:34 +0530

    done

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+
+const bodyParser = require("body-parser");
+
+const route = require("./route/route");
+
+const app = express();
+
+app.use(bodyParser.json());
+
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+
+mongoose
+  .connect(
+    "mongodb+srv://Satyaveer1994:Satyaveer123@cluster0.pn1nk.mongodb.net/satyaveer-DB",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("mongodb is connected"))
+  .catch((err) => console.log(err));
+
+app.use("/", route);
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Express app running on port " + (process.env.PORT || 3000));
+});
